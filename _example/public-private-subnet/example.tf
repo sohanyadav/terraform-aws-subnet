5ccbdeff7f2cd4e9654843e9d98c38f84f9cdd4b
Sohan Yadav <sohan@clouddrove.com>
2019-12-26 17:16:35 +0530

    change webhook

---
+++ _example/public-private-subnet/example.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vpc" {
+  source = "git::https://github.com/clouddrove/terraform-aws-vpc.git?ref=tags/0.12.4"
+
+  name        = "vpc"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+
+  cidr_block = "10.0.0.0/16"
+}
+
+module "subnets" {
+  source = "./../../"
+
+  name        = "subnets"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "application", "name"]
+
+  availability_zones  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  vpc_id              = module.vpc.vpc_id
+  type                = "public-private"
+  igw_id              = module.vpc.igw_id
+  nat_gateway_enabled = true
+  cidr_block          = module.vpc.vpc_cidr_block
+}
